25e1b6cd9ec2761deac48d614bd2c523bfa8502e
Greg Allen <mail@jga23.com>
2010-10-15 08:19:28 -0700

    added bindViewEvent to View.js

--- src/scripts/View.js
+++ src/scripts/View.js
@@ -2,8 +2,11 @@ var View = EventManager.extend({
 	init: function(elementId) {
 		this.element = document.getElementById(elementId);
 	},
-	bindElementEvent: function(selector, eventName, f) {
-		var elements = this.element.querySelectorAll(selector);
+	bindViewEvent: function(selector, eventName, f) {
+		this.bindElementEvent(this.element, selector, eventName, f);
+	},
+	bindElementEvent: function(element, selector, eventName, f) {
+		var elements = element.querySelectorAll(selector);
 		for (var i = 0; i < elements.length; i++) {
 			elements[i].addEventListener(eventName, f);
 		}
