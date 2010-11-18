afe63304658097edf01b9c93daa1e72780838fef
Greg Allen <mail@jga23.com>
2010-11-18 08:26:19 -0800

    elem support for array of elements

--- src/scripts/elem.js
+++ src/scripts/elem.js
@@ -4,9 +4,24 @@ var elem = {
 		return this.classRE(name).test(el.className);
 	},
 	addClass: function(el, name){
-		return !this.hasClass(el, name) && (el.className += (el.className ? ' ' : '') + name);
+		if (!el.length)
+			return !this.hasClass(el, name) && (el.className += (el.className ? ' ' : '') + name);
+		else {
+			for (var i = 0, c = el.length; i < c; i++) {
+				this.addClass(el[i], name);
+			}
+			return el;
+		}
+
 	},
 	removeClass: function(el, name){
-		return el.className = el.className.replace(this.classRE(name), '');
+		if (!el.length)
+			return el.className = el.className.replace(this.classRE(name), '');
+		else {
+			for (var i = 0, c = el.length; i < c; i++) {
+				this.removeClass(el[i], name);
+			}
+			return el;
+		}
 	}
 }
