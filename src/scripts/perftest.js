328fd3c4ba0aaf607b1da115b7dfc888deb984fa
Greg Allen <mail@jga23.com>
2010-12-05 09:32:41 -0800

    added PerfTest class

---
+++ src/scripts/perftest.js
@@ -0,0 +1,14 @@
+var PerfTest = Class.extend({
+	init: function(name) {
+		this.name = name;
+	},
+	start: function() {
+		this.startTime = new Date().getTime();
+		return this;
+	},
+	end: function() {
+		this.endTime = new Date().getTime();
+		console.log(String.format("PerfTest {0}: {1}", this.name, this.endTime - this.startTime));
+		return this;
+	}
+});
