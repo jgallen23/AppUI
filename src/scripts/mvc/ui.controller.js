226f9c3522e5d96252afb16e537e60f6fee41ca1
Greg Allen <mail@jga23.com>
2011-01-06 18:25:15 -0800

    processEvents on controller

--- src/scripts/mvc/ui.controller.js
+++ src/scripts/mvc/ui.controller.js
@@ -3,18 +3,27 @@ ui.Controller = ui.EventManager.extend({
 	init: function(element) {
 		this.view = new ui.View(element);
 		this.element = this.view.element;
+		this._processEvents = true;
 		if (this.useLiveClickEvents) {
             var self = this;
 			this.element.addEventListener(ui.INPUT_EVENT, this); 
         }
 	},
 	handleEvent: function(e) {
+		if (!this._processEvents)
+			return;
 		var self = this;
 		if (e.type == "click" || e.type == "touchdown") {
 			if (e.target.getAttribute('data-action') && self.actions[e.target.getAttribute("data-action")]) {
 				self.actions[e.target.getAttribute("data-action")].call(self, e);
 			}
 		}
+	},
+	enableEvents: function() {
+		this._processEvents = true;
+	},
+	disableEvents: function() {
+		this._processEvents = false;
 	},
 	destroy: function() {
 		if (this.useLiveClickEvents) {
