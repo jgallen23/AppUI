c11c7b9d88a26ed0af31da4779d75d056a0868c5
Greg Allen <mail@jga23.com>
2011-02-19 12:58:22 -0800

    prevent multiple events to fire

--- src/scripts/mvc/ui.controller.js
+++ src/scripts/mvc/ui.controller.js
@@ -6,13 +6,17 @@ ui.Controller = ui.EventManager.extend({
 		this._processEvents = true;
 		if (this.useLiveClickEvents) {
             var self = this;
-			this.view.element.addEventListener(ui.INPUT_EVENT, this); 
+			this.view.element.addEventListener(ui.INPUT_EVENT, this, false); 
         }
 	},
 	handleEvent: function(e) {
 		if (!this._processEvents)
 			return;
 		var self = this;
+		setTimeout(function() {
+			self._processEvents = true;
+		}, 100);
+		self._processEvents = false;
 		if (e.type == ui.INPUT_EVENT) {
 			var target = (e.target.nodeType == 1)?e.target:e.target.parentNode;
 			if (target.getAttribute('data-action') && self[target.getAttribute("data-action")]) {
@@ -28,14 +32,12 @@ ui.Controller = ui.EventManager.extend({
 	},
 	destroy: function() {
 		if (this.useLiveClickEvents) {
-			this.view.element.removeEventListener(ui.INPUT_EVENT, this);
+			this.view.element.removeEventListener(ui.INPUT_EVENT, this, false);
 		}
 		this._super();
 		this.trigger("destroy");
 	},
 	show: function() {
-		if (APP)
-			APP.currentController = this;
 		this.view.show();
 		this.trigger("show");
 	},
